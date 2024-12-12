0f0c166a4ec02766ad501a613cb8c423ee5fb729
Aditya yadav <adityayadavadi6100@gmail.com>
2024-12-12 12:50:20 +0530

    replace string

---
+++ script.js
@@ -0,0 +1,9 @@
+let mydata = `google is a great place to work.
+ google offers servies and google gives many amenities to the employees`;
+
+
+//i is for case-sensitive search g is for global searching
+let newData = mydata.replace(/google/ig,"amazon");
+console.log(newData);
+//BOM,DOM,VARIABLE,FUCTIONS ,ES6 FEATURES,OBJECTS,ARRAY,MATH METHOD,STRING METHOD
+//BOM=BROWSER OBJECT MODEL (WINDOW OBJECT).
